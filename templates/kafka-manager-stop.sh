a32af05e7d7325ff7164230186231c2516038097
polo <tinygg@163.com>
2019-07-17 21:45:41 +0800

    update desc

--- templates/kafka-manager-stop.sh
+++ templates/kafka-manager-stop.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+echo "stopping kafka-manager..."
 kafka_manager_pid=$(echo `ps aux | grep kafka-manager | awk '{ print $2 }'`)
 pid=$kafka_manager_pid
 echo "Shutting down Kafka Manager"
 kill -9 $pid
+
+exit 0
